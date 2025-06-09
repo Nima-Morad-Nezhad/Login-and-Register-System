8b6c183dcb1157ef5063b2476389ba72456bd38a
Nima-Morad-Nezhad <nmoradi.dev@gmail.com>
2025-06-09 03:08:01 +0300

    starting with register

---
+++ Partials/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login and Register System</title>
+    <link rel="stylesheet" href="/Project1/Login-and-Register-System/assets/style.css">
+</head>
+<body>
+  
